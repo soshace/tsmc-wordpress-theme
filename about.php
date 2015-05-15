67cd7e26987a46b0add3011b41427b884fd78103
Nikita <bragin_na@soshace.com>
2015-05-15 19:45:08 +0300

    Templates added

--- about.php
+++ about.php
@@ -1,9 +1,19 @@
+<?php
+/**
+ * Template Name: About
+ */
+
+?>
+
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <?php get_header(); ?>
 <body <?php body_class(); ?>>
 
 <?php wp_nav_menu('depth=1'); ?>
+
+<h1>Welcome to the standard market company</h1>
+<?php wp_nav_menu('submenu=about'); ?>
 
 <?php get_footer(); ?>
 </body>
--- caterings_category.php
+++ caterings_category.php
@@ -1,9 +1,19 @@
+<?php
+/**
+ * Template Name: Catering Category
+ */
+
+?>
+
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <?php get_header(); ?>
 <body <?php body_class(); ?>>
 
 <?php wp_nav_menu('depth=1'); ?>
+
+<h1>Departments</h1>
+<?php wp_nav_menu('submenu=departments'); ?>
 
 <?php get_footer(); ?>
 </body>
