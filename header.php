3ab4851f13c9e3aefb0c102d281f1176479cda45
Nikita <bragin_na@soshace.com>
2015-05-18 10:32:35 +0300

    Mobile version fix

--- header.php
+++ header.php
@@ -2,6 +2,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta charset="UTF-8">
     <title>The Standart Market Company</title>
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=yes"/>
     <!-- jquery -->
     <!-- Bootstrap Latest compiled and minified CSS -->
     <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/bootstrap.min.css">
